551edc6554a28f8d9a7fbd1371b6c9ead66260d2
Archangel45 <angelslashes@gmail.com>
2016-08-03 09:40:36 +0800

    Eleanor Rigby!

---
+++ madlib.py
@@ -0,0 +1,38 @@
+# A Madlib program that is from http://madlibs.org/cgi-bin/madlib?rigby.ml2
+
+print 'MadLibs: "Eleanor Rigby" by The Beatles'
+print 'Press Enter to begin'
+raw_input()
+
+print ''
+adjective = raw_input('Adjective: ')
+pluralNoun = raw_input('Plural Noun: ')
+femFirstName = raw_input('Femenine first name: ')
+lastName = raw_input('Last name: ')
+noun1 = raw_input('Noun: ')
+place = raw_input('A place: ')
+specialEvent = raw_input('A special event: ')
+verb1 = raw_input('Verb: ')
+noun2 = raw_input('Noun: ')
+presentVerb = raw_input('A verb in the present tense: ')
+noun3 = raw_input('Noun: ')
+noun4 = raw_input('Noun: ')
+noun5 = raw_input('Noun: ')
+noun6 = raw_input('Noun: ')
+verb2 = raw_input('Verb: ')
+
+print ''
+print 'Press Enter to confirm'
+raw_input()
+
+print ''
+print 'Ah, look at all the ' + adjective + ' ' + pluralNoun + '!'
+print 'Ah, look at all the ' + adjective + ' ' + pluralNoun + '!'
+print ''
+print femFirstName + ' picks up the ' + noun1 + ' in a ' + place + ' where a ' + specialEvent + ' has been.'
+print verb1 + ' in a ' + noun2 + '.'
+print ''
+print presentVerb + ' at the ' + noun3 + ', wearing the ' + noun4 + ' that the she keeps in a ' + noun5 + ' by the ' + noun6 + '. Who is it for?'
+print ''
+print 'All the ' + adjective + ' ' + pluralNoun + ', where do they all come from?'
+print 'All the ' + adjective + ' ' + pluralNoun + ', where do they ' + verb2 + '?'
